f6d12665bff635c7b32b37865cf415c434417941
Nouri-Chairet <nouric576@gmail.com>
2025-03-06 03:21:43 +0100

    adding a loading page

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -12,6 +12,10 @@ const Loader = () => {
 const StyledWrapper = styled.div`
   .loader {
     width: fit-content;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
     font-size: 50px;
     font-family: monospace;
     font-weight: bold;
